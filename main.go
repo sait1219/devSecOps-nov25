b30ea72c5dc9728fd1c51570df22a62c1c662f67
Keshav <anil.keshav@sap.com>
2025-11-21 19:07:18 +0100

    some message

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader((http.StatusOK))
-		fmt.Fprint(w, "I am alive and all okay")
+		fmt.Fprint(w, "I am alive and all okay and well")
 	})
 
 	log.Println("Server is on localhost at 8080")
